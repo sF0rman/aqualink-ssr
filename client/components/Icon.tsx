9521a2f5e09b1a337f4554d2d9323af79c11ebf1
Sebastian Forman <sebastian@forman.no>
2022-02-24 15:11:11 +0100

    Added mobile navigation drawer logic

--- client/components/Icon.tsx
+++ client/components/Icon.tsx
@@ -5,33 +5,37 @@ import {
   faArrowRight,
   faArrowUp,
   faEnvelope,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SizeProp } from "@fortawesome/fontawesome-svg-core";
 
-type Icon = "" | "arrowRight" | "arrowLeft" | "arrowUp" | "arrowDown" | "anchor" | "envelope";
+type Icon = "" | "arrowRight" | "arrowLeft" | "arrowUp" | "arrowDown" | "anchor" | "bars" | "envelope";
 
 interface IconProps {
   name: Icon;
   size?: SizeProp;
   className?: string;
+  onClick?: () => void;
 }
 
-const Icon = ({ name, size, className = "" }: IconProps) => {
+const Icon = ({ name, size, className = "", onClick }: IconProps) => {
   const getIcon = () => {
     switch (name) {
       case "arrowRight":
-        return <FontAwesomeIcon className={className} size={size} icon={faArrowRight} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faArrowRight} />;
       case "arrowLeft":
-        return <FontAwesomeIcon className={className} size={size} icon={faArrowLeft} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faArrowLeft} />;
       case "arrowUp":
-        return <FontAwesomeIcon className={className} size={size} icon={faArrowUp} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faArrowUp} />;
       case "arrowDown":
-        return <FontAwesomeIcon className={className} size={size} icon={faArrowDown} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faArrowDown} />;
       case "anchor":
-        return <FontAwesomeIcon className={className} size={size} icon={faAnchor} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faAnchor} />;
+      case "bars":
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faBars} />;
       case "envelope":
-        return <FontAwesomeIcon className={className} size={size} icon={faEnvelope} />;
+        return <FontAwesomeIcon onClick={onClick} className={className} size={size} icon={faEnvelope} />;
       default:
         return <></>;
     }
--- client/components/Navigation/Navigation.tsx
+++ client/components/Navigation/Navigation.tsx
@@ -1,13 +1,21 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Logo from "../../resources/logo-white.png";
 import NavItem from "./NavItem";
 import "./Navigation.scss";
+import Icon from "../Icon";
 
 const Navigation = (): ReactElement => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const toggleDrawer = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navigation">
-      <img src={Logo} alt="" />
-      <div className="nav-drawer">
+      <img src={Logo} className="main-logo" alt="" />
+      <div className={`nav-drawer ${menuOpen ? "open" : ""}`}>
+        <Icon name="bars" size="2x" className={`menuToggle mobile ${menuOpen ? "open" : ""}`} onClick={toggleDrawer} />
         <NavItem to="/">Home</NavItem>
         <NavItem to="/products">Products</NavItem>
         <NavItem to="/about">About AQUALINK</NavItem>
